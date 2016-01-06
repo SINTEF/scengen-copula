336fba530e220117e82b12d3761122b918df3fbf
mkaut <devnull@localhost>
2016-01-06 10:33:30 +0100

    made the fixed-margin method public; added tests

--- copula-sample.cpp
+++ copula-sample.cpp
@@ -41,7 +41,8 @@ double CopulaSample::gen_new_margin(DimT const marg)
 	DimT i, j, s;
 	DimT tg, iR;
 
-	assert (haveSc4Marg[marg] == false && "the margin has not been generated");
+	if (haveSc4Marg[marg])
+		throw std::logic_error("trying to generate an existing margin");
 
 	/// vector of target specifications for all the involved 2D copulas
 	std::vector<Cop2DInfo const*> tg2Dcopulas;
@@ -193,24 +194,28 @@ double CopulaSample::gen_sample()
 
 	marg = 0;
 	if (!haveSc4Marg[marg]) {
-        // initialize the first margin
-        for (s = 0; s < nSc; s++) {
-            /// \todo Once this works, test it with $nSc - s$ or random numbers.
-            /// \todo Make this work with random order, to add the possibility
-            ///       to shuffle scenarios. An alternative is to shuffle the
-            ///       output, but it is not so easy as we store the results in
-            ///       the wrong order (by margin, not by scenario).
-            /// \note For now, the values are shuffled at the end,
-            ///       using CopulaSample::shuffle_results().
-            sample[marg][s] = s; // alternative is to use random numbers
-        }
-        haveSc4Marg[marg] = true;
+		// initialize the first margin
+		for (s = 0; s < nSc; s++) {
+			/// \todo Once this works, test it with $nSc - s$ or random numbers.
+			/// \todo Make this work with random order, to add the possibility
+			///       to shuffle scenarios. An alternative is to shuffle the
+			///       output, but it is not so easy as we store the results in
+			///       the wrong order (by margin, not by scenario).
+			/// \note For now, the values are shuffled at the end,
+			///       using CopulaSample::shuffle_results().
+			sample[marg][s] = s; // alternative is to use random numbers
+		}
+		haveSc4Marg[marg] = true;
 	}
 
 	MSG (TrInfo, "Starting copula generation.")
 	for (marg = 1; marg < nVar; marg++) {
-		MSG (TrInfo, " - adding margin " << marg + 1 << " of " << nVar);
-		totDist += gen_new_margin(marg);
+		if (haveSc4Marg[marg]) {
+			MSG (TrInfo, " - skipping fixed margin " << marg + 1 << " of " << nVar);
+		} else {
+			MSG (TrInfo, " - adding margin " << marg + 1 << " of " << nVar);
+			totDist += gen_new_margin(marg);
+		}
 	}
 	MSG (TrInfo, "Finished generating the copula.")
 
@@ -354,23 +359,20 @@ void CopulaSample::shuffle_results()
 }
 
 
-void CopulaSample::fix_marg_values(MatrixI const & X, VectorI const & marg)
+void CopulaSample::fix_marg_values(MatrixI const & X)
 {
     DimT M = X.size1();
-    if (marg.size() > 0 && marg.size() != M)
-        throw std::length_error("The vector of margin indexes has different dimension than the provided matrix.");
     if (M > nVar)
         throw std::length_error("Trying to fix more margins than we have available.");
     if (X.size2() != nSc)
         throw std::length_error("Trying to fix margins with wrong number of scenarios.");
 
-    DimT i, m;
+    DimT m;
     for (m = 0; m < M; ++m) {
-        i = marg.size() > 0 ? marg[m] : m;
-        sample[i] = ublas::row(X, i);
-        if (haveSc4Marg[i])
+        if (haveSc4Marg[m])
             throw std::logic_error("Trying to fix an already fixed margin.");
-        assert(sample[i].size() == nSc && "dimension check");
-        haveSc4Marg[i] = true;
+        sample[m] = ublas::row(X, m);
+        assert(sample[m].size() == nSc && "dimension check");
+        haveSc4Marg[m] = true;
     }
 }
